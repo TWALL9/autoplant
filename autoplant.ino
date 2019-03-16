3f5eadb2be15292e82dcfbcb9a952fd62a58b081
tom wallis <thomaswallis0@gmail.com>
2019-03-16 17:22:06 -0400

    Initial commit

---
+++ autoplant.ino
@@ -0,0 +1,28 @@
+int moistureSensorPin = A0;
+int moistureThreshold = 25; // if the read moisture is lower than this, open the valve
+                             // value is between 0-100
+int moistureValue = 0; // the value read by the sensor
+int valvePin = 13;
+int valveOpenTime = 1000; // milliseconds to stay open
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(valvePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // read the moisture sensor value and convert some units
+  // the analogRead() function will return a number between 0 and 1023
+  // we want to work with 0-100 to make it easy, so we convert
+  // moistureValue/100 = x/1023 -> moistureValue = (x/1023)*100
+  
+  moistureValue = (analogRead(moistureSensorPin)/1023)*100;
+  
+  if (moistureValue < moistureThreshold){
+    digitalWrite(valvePin, HIGH); // open the valve
+    delay(valveOpenTime);
+    digitalWrite(valvePin, LOW); // close the valve
+  }
+}
